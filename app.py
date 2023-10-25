16cbed188b3d17afcb01d0ef6649bc435aa0a784
mohamed er-raouan <mohamader14@outlook.fr>
2023-10-25 02:29:45 +0200

    commit

--- app.py
+++ app.py
@@ -55,8 +55,8 @@ if __name__ == "__main__":
 
     alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
     l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
-
-    with mlflow.start_run(run_name="wine_quality"):
+    mlflow.set_experiment(experiment_name='wine_qualityyy')
+    with mlflow.start_run(run_name="model10"):
         lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
         lr.fit(train_x, train_y)
 
